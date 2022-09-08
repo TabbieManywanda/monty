8f4182a9baa442ebfe497e37ee28cdde652c5e8d
TabbieManywanda <achiengtabitha@gmail.com>
2022-09-08 14:40:05 -0700

    String functions

---
+++ stringfuncs.c
@@ -0,0 +1,88 @@
+#include "monty.h"
+
+/**
+ * _sch - search for char in string
+ * @s: string
+ * @c: char to find
+ *
+ * Return: 1 (success) or 0 if not
+ */
+int _sch(char *s, char c)
+{
+	int cont = 0;
+
+	while (s[cont] != '\0')
+	{
+		if (s[cont] == c)
+		{
+			break;
+		}
+		cont++;
+	}
+	if (s[cont] == c)
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string
+ * @s2: string2
+ *
+ * Return: 0 if s1 = s2 or any other number if different
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i] && s1[i]; i++)
+		;
+	if (s1[i] > s2[i])
+		return (1);
+	if (s1[i] < s2[i])
+		return (-1);
+	return (0);
+}
+
+/**
+ * _strtoky - cuts a string into tokens
+ * @s: string
+ * @d: delimiter
+ *
+ * Return: first partition
+ */
+char *_strtoky(char *s, char *d)
+{
+	static char *ultimo;
+	int i = 0, j = 0;
+
+	if (!s)
+		s = ultimo;
+	while (s[i] != '\0')
+	{
+		if (_sch(d, s[i]) == 0 && s[i + 1] == '\0')
+		{
+			ultimo = s + i + 1;
+			*ultimo = '\0';
+			s = s + j;
+			return (s);
+		}
+		else if (_sch(d, s[i]) == 0 && _sch(d, s[i + 1]) == 0)
+			i++;
+		else if (_sch(d, s[i]) == 0 && _sch(d, s[i + 1]) == 1)
+		{
+			ultimo = s + i + 1;
+			*ultimo = '\0';
+			ultimo++;
+			s = s + j;
+			return (s);
+		}
+		else if (_sch(d, s[i]) == 1)
+		{
+			j++;
+			i++;
+		}
+	}
+	return (NULL);
+}
